1642042af3b015925e74e83c06c132dcb3cb1d6d
tugul <tugul@live.com>
2016-09-26 15:30:07 +0200

    Recurring updates

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+
+//const platform = platformBrowserDynamic();
+//platform.bootstrapModule(AppModule);
+
+platformBrowserDynamic().bootstrapModule(AppModule);
